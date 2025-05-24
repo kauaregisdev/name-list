286f6ef33a9fb85b7b32ba02f3866661a2bd3600
Kauã Régis <kauarlc@hotmail.com>
2025-05-24 18:19:54 -0300

    fix: gender sem default

--- scripts/models.py
+++ scripts/models.py
@@ -3,4 +3,4 @@ from scripts.main import db
 class Name(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), nullable=False)
-    gender = db.Column(db.String(10), default='', nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
